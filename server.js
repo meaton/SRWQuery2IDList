47a613dfacf26a4df682afca434e5d8948c496f8
Mitchell Seaton <seaton@hum.ku.dk>
2014-03-12 23:17:43 +1100

    Cleanup express res

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 var app = require('express')(), fork = require('child_process').fork;
-
 app.get('/srwquery2idlist', function(req, res) {
     var query = fork(__dirname + '/srwquery2ids.js', ['-q', req.query.query, '-f', 'tsv', '-s']);
     var file_data = new String();
-
-    /*query.stdout.on('data', function(data) {
-	console.log('log from child: ' + data.toString());
-    });
-    query.stderr.on('data', function(data) {
-	console.log('child process => stderr: ' + data.toString());
-    });*/
     query.on('message', function(message) {
 	file_data += message;
     });
     query.on('exit', function(code) {
 	if(code == 0) {
-	    res.set({
-  		'Content-Type': 'text/tab-separated-values',
-  		'Content-Length': file_data.length});
+	    res.setHeader('Content-Type', 'text/tab-separated-values');
 	    res.send(new Buffer(file_data));
 	} else res.send(500, { error: 'error occurred!'});
     });
